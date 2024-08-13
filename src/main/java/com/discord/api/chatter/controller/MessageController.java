634457846d520b1ada2cee5c2e05a5e280113807
Josh Miller <josh.miller1994@yahoo.com>
2024-08-12 20:41:02 -0500

    Containerized and pulled scheduled code out

--- src/main/java/com/discord/api/chatter/controller/MessageController.java
+++ src/main/java/com/discord/api/chatter/controller/MessageController.java
@@ -4,14 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.discord.api.chatter.model.Webhook;
 import com.discord.api.chatter.service.DiscordService;
-
-import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class MessageController {
@@ -19,16 +16,10 @@ public class MessageController {
     @Autowired
     private DiscordService discordService;
 
-    @GetMapping("/")
-    public String homePage() {
-
-        return "index";
-    }
-
     @PostMapping(value = "/send-discord-message", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Webhook> sendMessageDiscordMessage(@RequestBody Webhook webhook, HttpServletResponse response) {
+    public ResponseEntity<Webhook> sendMessageDiscordMessage(@RequestBody Webhook webhook) {
         discordService.sendDiscordMessage(webhook);
-
+        System.out.println("Received webhook: " + webhook.toString());
         return new ResponseEntity<Webhook>(webhook, HttpStatus.OK);
     }
 }
